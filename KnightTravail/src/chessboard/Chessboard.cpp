fae6e6a3091098a46900873808899f69924001c8
Roko <roko@perica.com>
2022-01-09 19:10:07 +0100

    Added validation of chess notation

--- KnightTravail/src/chessboard/Chessboard.cpp
+++ KnightTravail/src/chessboard/Chessboard.cpp
@@ -4,7 +4,7 @@ namespace KnightTravail
 {
     ChessBoard::ChessBoard() {}
      
-    bool ChessBoard::inRange(const Coordinate& coordinate)
+    bool ChessBoard::inRangeInternal(const Coordinate& coordinate)
     {
         if (coordinate.x > boardSize-1 || coordinate.x < 0)
         {
@@ -17,6 +17,32 @@ namespace KnightTravail
         }
 
         return true;
+    }
+
+    bool ChessBoard::isValidChessNotationInternal(const std::string& chessNotation)
+    {
+        if (chessNotation.size() != 2)
+            return false;
+
+        const char letterNotation[8] = { 'a','b','c','d','e','f','g','h' };
+        const char numberNotation[8] = { '8','7','6','5','4','3','2','1' };
+
+        bool isValidLetter=false;
+        bool isValidNumber=false;
+
+        for (int i = 0; i < 8; i++)
+        {
+            if(!isValidLetter)
+                isValidLetter = chessNotation[0] == letterNotation[i];
+
+            if (!isValidNumber)
+                isValidNumber = chessNotation[1] == numberNotation[i];
+
+            if (isValidLetter && isValidNumber)
+                return true;
+        }
+
+        return false;
     }
 }
 
--- KnightTravail/src/chessboard/Chessboard.h
+++ KnightTravail/src/chessboard/Chessboard.h
@@ -4,15 +4,28 @@
 
 namespace KnightTravail
 {
+	//Singleton class
 	class ChessBoard
 	{
 	public:
 		static constexpr int boardSize=8;
 
 		ChessBoard(const ChessBoard&) = delete;
+		ChessBoard& operator=(const ChessBoard&) = delete;
+
+		static ChessBoard& Get()
+		{
+			static ChessBoard instance;
+			return instance;
+		}
+
+		static bool inRange(const Coordinate& coordinate) { return Get().inRangeInternal(coordinate); }
+		static bool isValidChessNotation(const std::string& chessNotation) { return Get().isValidChessNotationInternal(chessNotation); }
+
+	private:
 		ChessBoard();
 
-		static bool inRange(const Coordinate& coordinate);
-
+		bool inRangeInternal(const Coordinate& coordinate);
+		bool isValidChessNotationInternal(const std::string& chessNotation);
 	};
 }
