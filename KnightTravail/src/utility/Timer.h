7b27a6993f375180d431ce43ebd60af21328f72a
Roko <roko@perica.com>
2022-01-09 19:07:48 +0100

    Added timer

---
+++ KnightTravail/src/utility/Timer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <chrono>
+
+namespace utility
+{
+	class Timer
+	{
+	private:
+		using clock_t = std::chrono::high_resolution_clock;
+		using second_t = std::chrono::duration<double, std::ratio<1>>;
+
+		std::chrono::time_point<clock_t> m_beg{ clock_t::now() };
+
+	public:
+		void reset()
+		{
+			m_beg = clock_t::now();
+		}
+
+		double elapsed() const
+		{
+			return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+		}
+	};
+}
